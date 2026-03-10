f592ec49e8afa5d0f7f9a20af0b26fc9065a63d5
jadesensenat5-web <jadesensenat5@gmail.com>
2026-03-10 07:30:11 -0400

    ejercicio1

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1.Diccionario con tres elementos que representen un país y su capital.
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> paises = new Dictionary<string, string>();
+        paises.Add("República Dominicana", "Santo Domingo");
+        paises.Add("España", "Madrid");
+        paises.Add("México", "Ciudad de Mexico");
+        Console.WriteLine("--- Estas son las listas de mis Países y Capitales ---");
+        foreach (var item in paises)
+        {
+            Console.WriteLine("País: " + item.Key + " | Capital: " + item.Value);
+        }
+        Console.WriteLine("\nJades, presiona Enter para finalizar...");
+        Console.ReadLine();
+    }
+}
